983fbfed5e65ae60a11df6d629576b7db9e60f19
EddenJeong <77592371+EddenJeong@users.noreply.github.com>
2021-01-25 23:34:35 +0900

    add float

--- 2.4.c
+++ 2.4.c
@@ -5,4 +5,7 @@ int main(void)
 {
     int age = get_int("What's your age?\n");
     printf("You are at least %i days old.\n", age * 365);
+    
+    float price = get_float("What's the price?\n");
+    printf("Your total is %.2f.\n", price * 1.0625);
 }
