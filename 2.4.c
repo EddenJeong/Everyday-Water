64638ff15447500c89c705e879507e4491d2f724
EddenJeong <77592371+EddenJeong@users.noreply.github.com>
2021-01-25 23:42:46 +0900

    add parity

--- 2.4.c
+++ 2.4.c
@@ -5,7 +5,17 @@ int main(void)
 {
     int age = get_int("What's your age?\n");
     printf("You are at least %i days old.\n", age * 365);
-    
+
     float price = get_float("What's the price?\n");
     printf("Your total is %.2f.\n", price * 1.0625);
+
+    int n = get_int("n: ");
+    if (n % 2 == 0)
+    {
+        printf("even\n");
+    }
+    else if (n % 2 == 1)
+    {
+        printf("odd\n");
+    }
 }
