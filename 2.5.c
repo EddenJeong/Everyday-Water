434708a30b205e7547143c83372312c3bda65006
EddenJeong <77592371+EddenJeong@users.noreply.github.com>
2021-01-26 16:09:32 +0900

    Add row of bricks

--- 2.5.c
+++ 2.5.c
@@ -9,6 +9,18 @@ int main(void)
     cough(3);
     int i = get_positive_int();
     printf("%i\n", i);
+
+    int n;
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n < 1);
+    for (int j = 0; j < n; j++)
+    {
+        printf("?");
+    }
+    printf("\n");
 }
 
 
