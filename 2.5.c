89eb9a4c37045fd0f6363a10978b7f99c5e6a912
EddenJeong <77592371+EddenJeong@users.noreply.github.com>
2021-01-26 16:02:37 +0900

    Add get_postivie_int

--- 2.5.c
+++ 2.5.c
@@ -2,14 +2,29 @@
 #include <stdio.h>
 
 void cough(int n);
+int get_positive_int(void);
 
 int main(void)
 {
     cough(3);
+    int i = get_positive_int();
+    printf("%i\n", i);
 }
+
 
 void cough(int n)
 {
     for(int i = 0; i < 3; i++)
     printf("cough\n");
+}
+
+int get_positive_int(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Positive integer: ");
+    }
+    while (n < 1);
+    return n;
 }
